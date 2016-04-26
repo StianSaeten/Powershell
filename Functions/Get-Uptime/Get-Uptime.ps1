5a2c24c94bc3c303476a8c3a008e02b9420d3533
Stiansaeten <noreply@noreply.com>
2016-04-26 12:21:44 +0200

    Get-Uptime created

---
+++ Functions/Get-Uptime/Get-Uptime.ps1
@@ -0,0 +1,35 @@
+﻿#requires -Version 2
+function Get-Uptime
+{
+    <#
+            .SYNOPSIS
+            Get the latest boot time and calculate the uptime
+            .DESCRIPTION
+            Get the latest boot time and calculate the uptime
+            .EXAMPLE
+            Get-Uptime -ComputerName server1-cred domain\user
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = 'Please add a help message here')]
+        [Object]
+        $cred,
+        
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = 'Please add a help message here')]
+        [Object]
+        $ComputerName
+    )
+    
+    $os = Get-WmiObject -Class win32_operatingsystem -ComputerName $ComputerName -Credential $cred -ErrorAction SilentlyContinue
+    if ($os.LastBootUpTime) 
+    {
+        $uptime = (Get-Date) - $os.ConvertToDateTime($os.LastBootUpTime)
+        Write-Output -InputObject ('Last boot: ' + $os.ConvertToDateTime($os.LastBootUpTime) )
+        Write-Output -InputObject ('Uptime   : ' + $uptime.Days + ' Days ' + $uptime.Hours + ' Hours ' + $uptime.Minutes + ' Minutes' )
+    }
+    else 
+    {
+        Write-Warning -Message "Unable to connect to $ComputerName"
+    }
+}
