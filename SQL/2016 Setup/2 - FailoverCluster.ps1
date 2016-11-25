f6b0b1aefe982d53532c43d3a7cbb2916a2abffb
Stiansaeten <Stian@saeten.com>
2016-11-25 14:03:03 +0100

    daily

---
+++ SQL/2016 Setup/2 - FailoverCluster.ps1
@@ -0,0 +1,11 @@
+#Installere Failover Clustering
+
+
+Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools -ComputerName MON-DB15
+Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools -ComputerName MON-DB16
+
+Test-Cluster –Node MON-DB15
+Test-Cluster –Node MON-DB16
+
+New-Cluster –Name SQLAlwaysOn –Node MON-DB15, MON-DB16 –StaticAddress 10.10.110.30 -NoStorage
+
